a73242b4eafdebe1c9eebd7c668b2fa947d5dfe8
Federico 'Magus' Rocca <frocca17@gmail.com>
2019-11-11 20:53:25 -0300

    Se agrega funcionalidad de recupero de contraseña

---
+++ Utils/MailSender.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace Utils
+{
+    public class MailSender
+    {
+        private string User;
+        private string Password;
+
+        public void sendMail(string From, string To, string Subject, string Body)
+        {
+            try
+            {
+                var client = new SmtpClient("smtp.gmail.com", 587)
+                {
+                    Credentials = new NetworkCredential(User, Password),
+                    EnableSsl = true
+                };
+                MailMessage msg = new MailMessage(From, To, Subject, Body);
+                msg.IsBodyHtml = true;
+                client.Send(msg);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public MailSender(string _User, string _Password)
+        {
+            User = _User;
+            Password = _Password;
+        }
+    }
+}
