3c48e1ea12b83f5f34085265b3353ab42b501d99
Federico 'Magus' Rocca <frocca17@gmail.com>
2019-11-15 00:06:22 -0300

    Se agrega servicio de personas Se agrega validación de existencia de DNI

---
+++ ProgIII_EasyFitness_RoccaFederico/Service/PersonaService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataGateway;
+using ProgIII_EasyFitness_RoccaFederico.Models;
+
+namespace ProgIII_EasyFitness_RoccaFederico.Service
+{
+    public class PersonaService
+    {
+        public bool DNIExists(int _DNI)
+        {
+            try
+            {
+                DDBBGateway data = new DDBBGateway();
+                data.prepareQuery("select count(*) from Personas where dni = '" + _DNI + "';");
+                if (data.sendScalarQuery() <= 0)
+                {
+                    return false;
+                }
+                else return true;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void altaPersona( PersonaModel _Persona )
+        {
+            try
+            {
+                DDBBGateway userData = new DDBBGateway();
+                DDBBGateway personaData = new DDBBGateway();
+                userData.prepareQuery("insert into Usuarios values('" + _Persona.user.mail + "', '" + _Persona.user.password + "');");
+                userData.sendQuery();
+                personaData.prepareQuery("");
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
