e1b5cdbb14f4d7ea60c3d23b148492dc0f1355ec
Federico 'Magus' Rocca <frocca17@gmail.com>
2019-11-21 00:17:30 -0300

    Se rehacen relaciones de base de datos, debido a mal diseño

--- Database/Database_DDL.sql
+++ Database/Database_DDL.sql
@@ -14,15 +14,83 @@
 -- USE EasyFitness
 -- GO
 
+DROP TABLE IF EXISTS Teams
+DROP TABLE IF EXISTS Ejercicios
+DROP TABLE IF EXISTS Rutinas
+DROP TABLE IF EXISTS Entrenamientos
 DROP TABLE IF EXISTS Alumnos
 DROP TABLE IF EXISTS Entrenadores
-DROP TABLE IF EXISTS Personas
 DROP TABLE IF EXISTS Usuarios
-DROP TABLE IF EXISTS Entrenamientos
-DROP TABLE IF EXISTS Rutinas
-DROP TABLE IF EXISTS Ejercicios
-DROP TABLE IF EXISTS Teams
+DROP TABLE IF EXISTS Personas
 
+-- Creo en cascada:
+-- Persona
+-- Usuario
+-- Alumno
+-- Entrenadores
+-- Entrenamiento
+-- Rutina
+-- Ejercicio
+-- Teams
+
+-- Creo la tabla de personas
+CREATE TABLE Personas
+(
+	id BIGINT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	nombre VARCHAR(100) NOT NULL,
+	apellido VARCHAR(100) NOT NULL,
+	dni INT NOT NULL CHECK( DNI > 0 ),
+	fechaNacimiento DATETIME NOT NULL,
+	UNIQUE(dni)
+)
+
+-- Creo la tabla de usuarios
+CREATE TABLE Usuarios
+(
+	id BIGINT NOT NULL IDENTITY (1,1) PRIMARY KEY,
+	idPersona BIGINT NOT NULL FOREIGN KEY REFERENCES Personas(id),
+	mail VARCHAR(100) NOT NULL,
+	password VARCHAR(100) NOT NULL,
+	UNIQUE (id, mail)
+)
+
+-- Creo la tabla Alumnos
+CREATE TABLE Alumnos
+(
+	id BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	personaId BIGINT NOT NULL FOREIGN KEY REFERENCES Personas(id),
+	UNIQUE(personaId)
+)
+
+-- Creo la tabla de Entrenadores
+CREATE TABLE Entrenadores
+(
+	id BIGINT NOT NULL PRIMARY KEY IDENTITY (1,1),
+	personaId BIGINT NOT NULL FOREIGN KEY REFERENCES Personas(id)
+	UNIQUE(personaId)
+)
+
+-- Creo la tabla de entrenamientos
+CREATE TABLE Entrenamientos
+(
+	id BIGINT NOT NULL PRIMARY KEY IDENTITY (1,1),
+	idEntrenador BIGINT NOT NULL FOREIGN KEY REFERENCES Entrenadores(id),
+	idAlumno BIGINT NOT NULL FOREIGN KEY REFERENCES Alumnos(id),
+	descripcion VARCHAR(100) NOT NULL,
+	nombre VARCHAR(100) NOT NULL,
+	UNIQUE(descripcion, nombre, idEntrenador, idAlumno)
+)
+
+-- Creo la tabla de rutinas
+CREATE TABLE Rutinas
+(
+	id BIGINT NOT NULL IDENTITY (1,1) PRIMARY KEY,
+	idEntrenador BIGINT NOT NULL FOREIGN KEY REFERENCES Entrenadores(id),
+	idEntrenamiento BIGINT NOT NULL FOREIGN KEY REFERENCES Entrenamientos(id),
+	descripcion VARCHAR(100) NOT NULL,
+	nombre VARCHAR(100) NOT NULL,
+	UNIQUE(idEntrenamiento, idEntrenador, descripcion, nombre)
+)
 
 -- Creo la tabla de ejercicios
 CREATE TABLE Ejercicios
@@ -35,47 +103,8 @@ CREATE TABLE Ejercicios
 	repeticiones INT NULL,
 	comentarios VARCHAR(100) NOT NULL,
 	intensidad SMALLINT NOT NULL,
-)
-
--- Creo la tabla de rutinas
-CREATE TABLE Rutinas
-(
-	id BIGINT NOT NULL IDENTITY (1,1) PRIMARY KEY,
-	ejercicioId BIGINT NOT NULL FOREIGN KEY (ejercicioId) REFERENCES Ejercicios(id),
-	descripcion VARCHAR(100) NOT NULL,
-	nombre VARCHAR(100) NOT NULL,
-	UNIQUE(ejercicioId, descripcion, nombre)
-)
-
--- Creo la tabla de entrenamientos
-CREATE TABLE Entrenamientos
-(
-	id BIGINT NOT NULL PRIMARY KEY IDENTITY (1,1),
-	idRutina BIGINT NOT NULL FOREIGN KEY (idRutina) REFERENCES Rutinas(id),
-	descripcion VARCHAR(100) NOT NULL,
-	nombre VARCHAR(100) NOT NULL,
-	UNIQUE(idRutina, descripcion, nombre)
-)
-
--- Creo la tabla de usuarios
-CREATE TABLE Usuarios
-(
-	id BIGINT NOT NULL IDENTITY (1,1) PRIMARY KEY,
-	mail VARCHAR(100) NOT NULL,
-	password VARCHAR(100) NOT NULL,
-	UNIQUE (id, mail)
-)
-
--- Creo la tabla de personas
-CREATE TABLE Personas
-(
-	id BIGINT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	nombre VARCHAR(100) NOT NULL,
-	apellido VARCHAR(100) NOT NULL,
-	dni INT NOT NULL CHECK( DNI > 0 ),
-	fechaNacimiento DATETIME NOT NULL,
-	userID BIGINT NOT NULL FOREIGN KEY (userID) REFERENCES Usuarios(id),
-	UNIQUE(dni)
+	idEntrenador BIGINT NOT NULL FOREIGN KEY REFERENCES Entrenadores(id),
+	idRutina BIGINT NOT NULL FOREIGN KEY REFERENCES Rutinas(id),
 )
 
 -- Creo la tabla de Teams
@@ -84,26 +113,8 @@ CREATE TABLE Teams
 	id BIGINT NOT NULL PRIMARY KEY IDENTITY (1,1),
 	nombre VARCHAR(100) NOT NULL,
 	descripcion VARCHAR(100) NOT NULL,
-	UNIQUE(nombre, descripcion)
-)
-
--- Creo la tabla Alumnos
-CREATE TABLE Alumnos
-(
-	id BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	personaId BIGINT NOT NULL FOREIGN KEY (personaId) REFERENCES Personas(id),
-	entrenamientoID BIGINT NULL FOREIGN KEY (entrenamientoID) REFERENCES Entrenamientos(ID),
-	teamID BIGINT NULL FOREIGN KEY (teamID) REFERENCES Teams(ID),
-	UNIQUE(entrenamientoID, teamID)
-)
-
-CREATE TABLE Entrenadores
-(
-	id BIGINT NOT NULL PRIMARY KEY IDENTITY (1,1),
-	personaId BIGINT NOT NULL FOREIGN KEY (personaId) REFERENCES Personas(id),
-	entrenamientoId BIGINT NULL FOREIGN KEY (entrenamientoId) REFERENCES Entrenamientos(id),
-	rutinaId BIGINT NULL FOREIGN KEY (rutinaId) REFERENCES Rutinas(id),
-	ejercicioId BIGINT NULL FOREIGN KEY (ejercicioId) REFERENCES Ejercicios(id),
-	teamId BIGINT NULL FOREIGN KEY (teamId) REFERENCES Teams(id),
-	UNIQUE(entrenamientoId, rutinaId, ejercicioId, teamId)
+	idEntrenador BIGINT NOT NULL FOREIGN KEY REFERENCES Entrenadores(id),
+	idEntrenamiento BIGINT NOT NULL FOREIGN KEY REFERENCES Entrenamientos(id),
+	idAlumno BIGINT NOT NULL FOREIGN KEY REFERENCES Alumnos(id),
+	UNIQUE(idEntrenador, idEntrenamiento, idAlumno)
 )
--- Database/Database_DML.sql
+++ Database/Database_DML.sql
@@ -1,106 +1,86 @@
-﻿USE [EasyFitness_DB]
+﻿SET IDENTITY_INSERT [dbo].[Personas] ON 
 GO
-SET IDENTITY_INSERT [dbo].[Ejercicios] ON 
+INSERT [dbo].[Personas] ([id], [nombre], [apellido], [dni], [fechaNacimiento]) VALUES (1, N'Nombre 1', N'Apellido 1', 123, CAST(N'1998-01-02T00:00:00.000' AS DateTime))
 GO
-INSERT [dbo].[Ejercicios] ([id], [nombre], [tipo], [urlEjemplo], [tiempo], [repeticiones], [comentarios], [intensidad]) VALUES (1, N'ejerc1', 'Tipo1', N'1', 1, 1, N'1', 1)
+INSERT [dbo].[Personas] ([id], [nombre], [apellido], [dni], [fechaNacimiento]) VALUES (2, N'Nombre 2', N'Apellido 2', 234, CAST(N'1998-01-02T00:00:00.000' AS DateTime))
 GO
-INSERT [dbo].[Ejercicios] ([id], [nombre], [tipo], [urlEjemplo], [tiempo], [repeticiones], [comentarios], [intensidad]) VALUES (2, N'ejerc2', 'Tipo2', N'2', 2, 2, N'2', 2)
-GO
-INSERT [dbo].[Ejercicios] ([id], [nombre], [tipo], [urlEjemplo], [tiempo], [repeticiones], [comentarios], [intensidad]) VALUES (3, N'ejerc3', 'Tipo3', N'3', 3, 3, N'3', 3)
-GO
-SET IDENTITY_INSERT [dbo].[Ejercicios] OFF
-GO
-SET IDENTITY_INSERT [dbo].[Rutinas] ON 
-GO
-INSERT [dbo].[Rutinas] ([id], [ejercicioId], [descripcion], [nombre]) VALUES (1, 1, N'1', N'1')
-GO
-INSERT [dbo].[Rutinas] ([id], [ejercicioId], [descripcion], [nombre]) VALUES (2, 2, N'2', N'2')
-GO
-INSERT [dbo].[Rutinas] ([id], [ejercicioId], [descripcion], [nombre]) VALUES (3, 3, N'3', N'3')
-GO
-SET IDENTITY_INSERT [dbo].[Rutinas] OFF
-GO
-SET IDENTITY_INSERT [dbo].[Entrenamientos] ON 
-GO
-INSERT [dbo].[Entrenamientos] ([id], [idRutina], [descripcion], [nombre]) VALUES (1, 1, N'1', N'1')
-GO
-INSERT [dbo].[Entrenamientos] ([id], [idRutina], [descripcion], [nombre]) VALUES (2, 2, N'2', N'2')
-GO
-INSERT [dbo].[Entrenamientos] ([id], [idRutina], [descripcion], [nombre]) VALUES (3, 3, N'3', N'3')
-GO
-SET IDENTITY_INSERT [dbo].[Entrenamientos] OFF
-GO
-SET IDENTITY_INSERT [dbo].[Usuarios] ON 
-GO
-INSERT [dbo].[Usuarios] ([id], [mail], [password]) VALUES (1, N'1', N'1')
-GO
-INSERT [dbo].[Usuarios] ([id], [mail], [password]) VALUES (2, N'2', N'2')
-GO
-INSERT [dbo].[Usuarios] ([id], [mail], [password]) VALUES (3, N'3', N'3')
-GO
-INSERT [dbo].[Usuarios] ([id], [mail], [password]) VALUES (4, N'4', N'4')
-GO
-INSERT [dbo].[Usuarios] ([id], [mail], [password]) VALUES (5, N'5', N'5')
-GO
-SET IDENTITY_INSERT [dbo].[Usuarios] OFF
-GO
-SET IDENTITY_INSERT [dbo].[Personas] ON 
-GO
-INSERT [dbo].[Personas] ([id], [nombre], [apellido], [dni], [fechaNacimiento], [userID]) VALUES (1, N'1', N'1', 1, CAST(N'1998-01-02T00:00:00.000' AS DateTime), 1)
-GO
-INSERT [dbo].[Personas] ([id], [nombre], [apellido], [dni], [fechaNacimiento], [userID]) VALUES (2, N'2', N'2', 2, CAST(N'1998-01-02T00:00:00.000' AS DateTime), 2)
-GO
-INSERT [dbo].[Personas] ([id], [nombre], [apellido], [dni], [fechaNacimiento], [userID]) VALUES (3, N'3', N'3', 3, CAST(N'1998-01-02T00:00:00.000' AS DateTime), 3)
+INSERT [dbo].[Personas] ([id], [nombre], [apellido], [dni], [fechaNacimiento]) VALUES (3, N'Nombre 3', N'Apellido 3', 345, CAST(N'1998-01-02T00:00:00.000' AS DateTime))
 GO
 SET IDENTITY_INSERT [dbo].[Personas] OFF
 GO
-SET IDENTITY_INSERT [dbo].[Teams] ON 
+SET IDENTITY_INSERT [dbo].[Usuarios] ON 
 GO
-INSERT [dbo].[Teams] ([id], [nombre], [descripcion]) VALUES (1, N'1', N'1')
+INSERT [dbo].[Usuarios] ([id], [idPersona], [mail], [password]) VALUES (1, 1, N'mail1@mail.com', N'password1')
 GO
-INSERT [dbo].[Teams] ([id], [nombre], [descripcion]) VALUES (2, N'2', N'2')
+INSERT [dbo].[Usuarios] ([id], [idPersona], [mail], [password]) VALUES (2, 2, N'mail2@mail2.com', N'password2')
 GO
-INSERT [dbo].[Teams] ([id], [nombre], [descripcion]) VALUES (3, N'3', N'3')
+INSERT [dbo].[Usuarios] ([id], [idPersona], [mail], [password]) VALUES (3, 3, N'mail3@mail3.com', N'password3')
 GO
-INSERT [dbo].[Teams] ([id], [nombre], [descripcion]) VALUES (4, N'4', N'4')
-GO
-INSERT [dbo].[Teams] ([id], [nombre], [descripcion]) VALUES (5, N'5', N'5')
-GO
-INSERT [dbo].[Teams] ([id], [nombre], [descripcion]) VALUES (6, N'6', N'6')
-GO
-SET IDENTITY_INSERT [dbo].[Teams] OFF
-GO
-SET IDENTITY_INSERT [dbo].[Entrenadores] ON 
-GO
-INSERT [dbo].[Entrenadores] ([id], [personaId], [entrenamientoId], [rutinaId], [ejercicioId], [teamId]) VALUES (1, 1, 1, 1, 1, 1)
-GO
-INSERT [dbo].[Entrenadores] ([id], [personaId], [entrenamientoId], [rutinaId], [ejercicioId], [teamId]) VALUES (2, 2, 2, 2, 2, 2)
-GO
-INSERT [dbo].[Entrenadores] ([id], [personaId], [entrenamientoId], [rutinaId], [ejercicioId], [teamId]) VALUES (4, 3, 3, 3, 3, 3)
-GO
-SET IDENTITY_INSERT [dbo].[Entrenadores] OFF
+SET IDENTITY_INSERT [dbo].[Usuarios] OFF
 GO
 SET IDENTITY_INSERT [dbo].[Alumnos] ON 
 GO
-INSERT [dbo].[Alumnos] ([id], [personaId], [entrenamientoID], [teamID]) VALUES (2, 1, 1, NULL)
+INSERT [dbo].[Alumnos] ([id], [personaId]) VALUES (1, 1)
 GO
-INSERT [dbo].[Alumnos] ([id], [personaId], [entrenamientoID], [teamID]) VALUES (4, 2, NULL, NULL)
+INSERT [dbo].[Alumnos] ([id], [personaId]) VALUES (2, 2)
 GO
-INSERT [dbo].[Alumnos] ([id], [personaId], [entrenamientoID], [teamID]) VALUES (5, 3, NULL, 3)
-GO
-INSERT [dbo].[Alumnos] ([id], [personaId], [entrenamientoID], [teamID]) VALUES (10, 2, 2, 2)
-GO
-INSERT [dbo].[Alumnos] ([id], [personaId], [entrenamientoID], [teamID]) VALUES (11, 2, 1, 2)
-GO
-INSERT [dbo].[Alumnos] ([id], [personaId], [entrenamientoID], [teamID]) VALUES (12, 2, 2, 1)
-GO
-INSERT [dbo].[Alumnos] ([id], [personaId], [entrenamientoID], [teamID]) VALUES (13, 2, 3, 1)
-GO
-INSERT [dbo].[Alumnos] ([id], [personaId], [entrenamientoID], [teamID]) VALUES (14, 2, 3, 3)
-GO
-INSERT [dbo].[Alumnos] ([id], [personaId], [entrenamientoID], [teamID]) VALUES (17, 2, 3, 4)
-GO
-INSERT [dbo].[Alumnos] ([id], [personaId], [entrenamientoID], [teamID]) VALUES (18, 2, 3, 5)
+INSERT [dbo].[Alumnos] ([id], [personaId]) VALUES (3, 3)
 GO
 SET IDENTITY_INSERT [dbo].[Alumnos] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Entrenadores] ON 
+GO
+INSERT [dbo].[Entrenadores] ([id], [personaId]) VALUES (1, 1)
+GO
+INSERT [dbo].[Entrenadores] ([id], [personaId]) VALUES (2, 2)
+GO
+INSERT [dbo].[Entrenadores] ([id], [personaId]) VALUES (3, 3)
+GO
+SET IDENTITY_INSERT [dbo].[Entrenadores] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Entrenamientos] ON 
+GO
+INSERT [dbo].[Entrenamientos] ([id], [idEntrenador], [idAlumno], [descripcion], [nombre]) VALUES (1, 1, 1, N'Descripcion 1', N'Entrenamiento 1')
+GO
+INSERT [dbo].[Entrenamientos] ([id], [idEntrenador], [idAlumno], [descripcion], [nombre]) VALUES (2, 2, 2, N'Descripcion 2', N'Entrenamiento 2')
+GO
+INSERT [dbo].[Entrenamientos] ([id], [idEntrenador], [idAlumno], [descripcion], [nombre]) VALUES (3, 3, 3, N'Descripcion 3', N'Entrenamiento 3')
+GO
+INSERT [dbo].[Entrenamientos] ([id], [idEntrenador], [idAlumno], [descripcion], [nombre]) VALUES (4, 1, 1, N'Descripcion 4', N'Entrenamiento 4')
+GO
+INSERT [dbo].[Entrenamientos] ([id], [idEntrenador], [idAlumno], [descripcion], [nombre]) VALUES (5, 1, 3, N'Descripcion 5', N'Entrenamiento 5')
+GO
+SET IDENTITY_INSERT [dbo].[Entrenamientos] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Teams] ON 
+GO
+INSERT [dbo].[Teams] ([id], [nombre], [descripcion], [idEntrenador], [idEntrenamiento], [idAlumno]) VALUES (1, N'Team 1', N'Descripcion 1', 1, 1, 1)
+GO
+INSERT [dbo].[Teams] ([id], [nombre], [descripcion], [idEntrenador], [idEntrenamiento], [idAlumno]) VALUES (3, N'Team 2', N'Descripcion 2', 2, 2, 2)
+GO
+INSERT [dbo].[Teams] ([id], [nombre], [descripcion], [idEntrenador], [idEntrenamiento], [idAlumno]) VALUES (4, N'Team 3', N'Descripcion 3', 3, 3, 3)
+GO
+SET IDENTITY_INSERT [dbo].[Teams] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Rutinas] ON 
+GO
+INSERT [dbo].[Rutinas] ([id], [idEntrenador], [idEntrenamiento], [descripcion], [nombre]) VALUES (1, 1, 1, N'Descripcion 1', N'Nombre 1')
+GO
+INSERT [dbo].[Rutinas] ([id], [idEntrenador], [idEntrenamiento], [descripcion], [nombre]) VALUES (2, 2, 2, N'Descripcion 2', N'Nombre 2')
+GO
+INSERT [dbo].[Rutinas] ([id], [idEntrenador], [idEntrenamiento], [descripcion], [nombre]) VALUES (4, 1, 3, N'Descripcion 4', N'Nombre 4')
+GO
+INSERT [dbo].[Rutinas] ([id], [idEntrenador], [idEntrenamiento], [descripcion], [nombre]) VALUES (3, 3, 3, N'Descripcion 3', N'Nombre 3')
+GO
+SET IDENTITY_INSERT [dbo].[Rutinas] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Ejercicios] ON 
+GO
+INSERT [dbo].[Ejercicios] ([id], [nombre], [tipo], [urlEjemplo], [tiempo], [repeticiones], [comentarios], [intensidad], [idEntrenador], [idRutina]) VALUES (1, N'Nombre 1', N'Tipo 1', N'URL 1', 1, 1, N'Comentarios 1', 1, 1, 1)
+GO
+INSERT [dbo].[Ejercicios] ([id], [nombre], [tipo], [urlEjemplo], [tiempo], [repeticiones], [comentarios], [intensidad], [idEntrenador], [idRutina]) VALUES (3, N'Nombre 2', N'Tipo 2', N'URL 2', 2, 2, N'Comentarios 2', 2, 2, 2)
+GO
+INSERT [dbo].[Ejercicios] ([id], [nombre], [tipo], [urlEjemplo], [tiempo], [repeticiones], [comentarios], [intensidad], [idEntrenador], [idRutina]) VALUES (5, N'Nombre 3', N'Tipo 3', N'URL 3', 3, 3, N'Comentarios 3', 3, 3, 3)
+GO
+SET IDENTITY_INSERT [dbo].[Ejercicios] OFF
 GO
